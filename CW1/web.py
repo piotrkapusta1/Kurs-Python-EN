7bc7ea6edcbcbe9b51c8a5154e7a35abf846829a
Piotr <piokapusta@gmail.com>
2023-03-05 12:31:01 +0100

    Add simple web app with streamlit

---
+++ CW1/web.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo + '\n')
+    functions.write_todos(todos)
+
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to your productivity.")
+
+for todo in todos:
+    st.checkbox(todo)
+    
+st.text_input(label="",
+               placeholder="Add new todo...",
+               on_change=add_todo,
+               key='new_todo')
+
+st.session_state
