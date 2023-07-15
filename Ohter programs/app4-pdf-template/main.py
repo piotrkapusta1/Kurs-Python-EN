aa0d791be52caf01c79826bd270e449c07984f27
Piotr Kap <piokapusta@gmail.com>
2023-07-15 22:23:20 +0200

    Add lines to pdf

--- Ohter programs/app4-pdf-template/main.py
+++ Ohter programs/app4-pdf-template/main.py
@@ -15,6 +15,9 @@ for index, row in df.iterrows():
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0,h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10,21,200,21)
+    for i in range(20,300,10):
+        pdf.line(10,i,200,i)
+
 
 
     #footer
@@ -30,6 +33,8 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style = "I", size=8)
         pdf.set_text_color(180,180,180)
         pdf.cell(w=0,h = 10, txt=row["Topic"],align="R")
+        for i in range(20,300,10):
+            pdf.line(10,i,200,i)
 
 
 pdf.output("output.pdf")
