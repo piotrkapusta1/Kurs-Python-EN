6e377f21a795bd75ab7475b37ee8402933260b8c
Piotr Kap <piokapusta@gmail.com>
2023-07-17 21:21:23 +0200

    Get message from api update

--- CW3/main.py
+++ CW3/main.py
@@ -1,7 +1,7 @@
 import requests
 
-api_key = "3d7407d176d948ab99287ac3fbc017bc"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-06-17&sortBy=publishedAt&apiKey=3d7407d176d948ab99287ac3fbc017bc"
+api_key = ""
+url = "https://newsapi.org/v2/everything?q=tesla&from=2023-06-17&sortBy=publishedAt&apiKey="
 
 request = requests.get(url)
 
